822c3e9577b15b50c89d7c9a589dda35b46b8e41
Pedro Otavio <pedromartins06072008@gmail.com>
2025-05-07 18:14:05 -0300

    EXEMPLO 1: criação da função mensagem

---
+++ main.js
@@ -0,0 +1,14 @@
+//A função bloce ne codigo que peseroenno ve popet especifico que se sera executono quando for chamado
+
+
+/*
+EXEMPLO 1 
+
+function  menssagem(){
+    alert("Seja muito bem-vindo(a)!")
+    console.log(" Mais uma vez,  seja muito bem-vindo(a) novamente!")
+}
+
+menssagem() 
+*/
+
